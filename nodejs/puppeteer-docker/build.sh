c37cd4e50017c13458282c7cb123a66735700f14
Mengjun Liu <mengjun_liu@126.com>
2019-03-02 19:05:38 +0800

    Add Puppeteer docker config

---
+++ nodejs/puppeteer-docker/build.sh
@@ -0,0 +1,3 @@
+# docker build -t node-puppeteer:puppeteer1_12-node10-alpine .
+# docker build -t node-puppeteer:puppeteer1_9-node10-alpine .
+docker build -t node-puppeteer:puppeteer1_11-node10-alpine .
